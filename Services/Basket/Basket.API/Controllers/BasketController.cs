5b88de2c79fb88f5b7f65e04b944b8118ae5e36d
Samuel Louis Sampeur <sampeur4life1@gmail.com>
2021-09-24 14:45:19 -0700

    Complete CRUD of Discount API and Discount Grpc projects

--- Services/Basket/Basket.API/Controllers/BasketController.cs
+++ Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -28,31 +28,32 @@ namespace Basket.API.Controllers
 
         [HttpGet("{userName}", Name = "GetBasket")]
         [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
-        //public async Task<ActionResult<ShoppingCart>> GetBasket(string userName)
-        //{
-        //    //var basket = await _repository.GetBasket(userName);
-        //    //return Ok(basket ?? new ShoppingCart(userName));
-        //}
+        public async Task<ActionResult<ShoppingCart>> GetBasket(string userName)
+        {
+            var basket = await _repository.GetBasket(userName);
+            return Ok(basket ?? new ShoppingCart(userName));
+        }
 
-        //[HttpPost]
-        //[ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
-        //public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody] ShoppingCart basket)
-        //{
-        //    // Communicate with Discount.Grpc and calculate lastest prices of products into sc
-        //    foreach (var item in basket.Items)
-        //    {
-        //        var coupon = await _discountGrpcService.GetDiscount(item.ProductName);
-        //        item.Price -= coupon.Amount;
-        //    }
+        [HttpPost]
+        [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody] ShoppingCart basket)
+        {
+            // Communicate with Discount.Grpc and calculate lastest prices of products into sc
 
-        //    return Ok(await _repository.UpdateBasket(basket));
-        //}
+            //foreach (var item in basket.Items)
+            //{
+            //    var coupon = await _discountGrpcService.GetDiscount(item.ProductName);
+            //    item.Price -= coupon.Amount;
+            //}
+
+            return Ok(await _repository.UpdateBasket(basket));
+        }
 
         [HttpDelete("{userName}", Name = "DeleteBasket")]
         [ProducesResponseType(typeof(void), (int)HttpStatusCode.OK)]
         public async Task<IActionResult> DeleteBasket(string userName)
         {
-            //await _repository.DeleteBasket(userName);
+            await _repository.DeleteBasket(userName);
             return Ok();
         }
 
