ae1d6d5d3f93073fab874efc4db2ec2507ca6e9f
Samuel Louis Sampeur <sampeur4life1@gmail.com>
2021-12-17 21:20:19 -0800

    scaffolded identity for account managing pages

---
+++ WebApps/AspnetRunBasics/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using AspnetRunBasics.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(AspnetRunBasics.Areas.Identity.IdentityHostingStartup))]
+namespace AspnetRunBasics.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AspnetRunBasicsContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AspnetRunBasicsContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<AspnetRunBasicsContext>();
+            });
+        }
+    }
+}
