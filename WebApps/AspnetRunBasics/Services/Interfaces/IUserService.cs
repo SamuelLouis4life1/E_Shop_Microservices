bd42c15697c6107a80f24ae8045656c6e1916745
Samuel Louis Sampeur <sampeur4life1@gmail.com>
2021-12-21 06:18:06 -0800

    Created project CRUD User authentication with JWT

---
+++ WebApps/AspnetRunBasics/Services/Interfaces/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspnetRunBasics.Services.Interfaces
+{
+    public interface IUserService
+    {
+        //Task<UserModel> GetUser(string userName);
+        //Task<UserModel> CreateUser(UserModel userModel);
+        //Task<UserModel> UpdateUser(UserModel userModel);
+        Task DeleteUser(string userName);
+    }
+}
